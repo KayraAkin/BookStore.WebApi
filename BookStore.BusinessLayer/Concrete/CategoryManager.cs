76c3067d45bc77966b98c2dd6403e27c880716b3
kayra akın <kayrakin11@hotmail.com>
2025-02-14 17:45:14 +0300

    update

---
+++ BookStore.BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,49 @@
+﻿using BookStore.BusinessLayer.Abstract;
+using BookStore.DataAccessLayer.Abstract;
+using BookStore.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategorDal _categoryDal;
+
+        public CategoryManager(ICategorDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public void TAdd(Category entity)
+        {
+            if (entity.CategoryName.Length >= 3 && entity.CategoryName.Length <= 30 && entity.CategoryName.Contains('a')) 
+            {
+                _categoryDal.Add(entity);
+            }
+            //hata mesajı
+        }
+
+        public void TDelete(int id)
+        {
+            _categoryDal.Delete(id);
+        }
+
+        public List<Category> TGetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public Category TGetByID(int id)
+        {
+             return _categoryDal.GetByID(id);
+        }
+
+        public void TUpdate(Category entity)
+        {
+            _categoryDal.Update(entity);
+        }
+    }
+}
