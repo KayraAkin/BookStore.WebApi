68bb70ca14d82a9656887050e4e46e713995dc12
kayra akın <kayrakin11@hotmail.com>
2025-02-14 16:05:32 +0300

    Add project files.

---
+++ BookStore.DataAccessLayer/Context/BookStoreContext.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccessLayer.Context
+{
+    public class BookStoreContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-Q1QVO4K\\SQLEXPRESS;initial catalog = ApiBookDb;integrated security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
